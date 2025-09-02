06266637df4d46f1f8629f8fe785e22e616aeb6a
MRP-rakib <rakib260.hossain@gmail.com>
2025-09-03 03:19:38 +0600

    change on user page

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,7 +3,7 @@ import UserPage from '../../components/UserPage'
 
 export default function Page() {
   return (
-    <Suspense fallback={<div className="flex items-center justify-center text-4xl h-[100vh]">Loading...</div>}>
+    <Suspense>
       <UserPage />
     </Suspense>
   )
