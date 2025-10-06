ff02dca57746a2193fb650922fa2729d4fd048d7
MRP-rakib <rakib260.hossain@gmail.com>
2025-10-07 02:42:02 +0600

    first commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav className='sticky top-0 left-0 w-full h-[50px] bg-red-400'>
+
+            Navbar
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/navbar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <aside className=' w-[200px] h-screen sticky top-0 left-0 bg-red-400'>
+            Sidebar
+        </aside>
+    )
+}
+
+export default Sidebar
