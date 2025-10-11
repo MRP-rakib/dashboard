fec2576c3ea10cf6f14aaab20852322607a054b0
MRP-rakib <rakib260.hossain@gmail.com>
2025-10-12 04:52:36 +0600

    fixed some error

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { IoIosMenu } from "react-icons/io";
 function Navbar() {
     const dispatch = useAppDispatch()
     return (
-        <nav className='sticky top-0 left-0 w-full py-6 border-b border-gray-20 z-50 bg-white'>
+        <nav className='sticky top-0 left-0 w-full py-6 border-b border-gray-200 z-50 bg-white'>
             <Container>
                 <div>
                     <button onClick={()=>dispatch(toggleSidebar())} className='text-2xl text-[#10B981] cursor-pointer'><IoIosMenu /></button>
